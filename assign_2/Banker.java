82b21bbc1ef775a71118a53d8336f23154c6b807
musturuharitha <84260808+musturuharitha@users.noreply.github.com>
2021-09-09 17:26:33 +0530

    Add files via upload

---
+++ assign_2/Banker.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class Banker {
+	
+
+	public class bankmainmethod {
+
+		public void main(String[] args) {
+			
+			banker s1=new savings(1,hari",1000);"
+			banker c1=new current(2,"sai",15000);
+			System.out.println("bal of s1= "+s1.getBalance());
+			System.out.println("bal of c1= "+c1.getBalance());
+			System.out.println("total cash in bank= "+(s1.getBalance()+c1.getBalance()));
+		}
+
+}
+}
