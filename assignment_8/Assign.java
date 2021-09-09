76c9499d2c11a9298a26fa832fa96d01fe394841
musturuharitha <84260808+musturuharitha@users.noreply.github.com>
2021-09-09 17:11:26 +0530

    Add files via upload

---
+++ assignment_8/Assign.java
@@ -0,0 +1,37 @@
+package assign_8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+
+ class Assign {
+
+	public static void main(String[] args) {
+	
+		
+		class A8_q0{
+
+		   
+
+		        List<String> list1 = new ArrayList<String>();
+		
+		
+
+		 
+		        list1.add("capgemini");
+		        list2.add("hi hari");
+		        list3.add("good evening");
+		        
+		        list1.removeIf(s->s.length()%2!=0);
+
+		        list1.forEach(System.out::println);
+
+	
+		}
+
+	
+
+}
+ }
+ 
